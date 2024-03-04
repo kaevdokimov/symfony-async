62c783d1d963f657b4ae50530bb6365ac8ad5505
kevdokimov <79169605859@ya.ru>
2024-03-04 22:11:48 +0300

    Create PurchaseConfirmationNotification

---
+++ src/Message/PurchaseConfirmationNotification.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Message;
+
+readonly class PurchaseConfirmationNotification
+{
+
+    public function __construct(private object $order)
+    {
+    }
+
+    public function getOrder(): object
+    {
+        return $this->order;
+    }
+
+}
