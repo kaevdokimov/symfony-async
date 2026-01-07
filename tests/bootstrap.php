224ce279bea9996b98203e066053dae3d14afab9
Kirill Evdokimov <22015558+kaevdokimov@users.noreply.github.com>
2026-01-08 00:48:22 +0300

    Update PHPUnit version to 11.5 in symfony.lock, adjust .gitignore for PHPUnit cache, and clean up bootstrap.php by removing unused error handler

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,8 @@
 <?php
-declare(strict_types=1);
 
-use Symfony\Component\ErrorHandler\ErrorHandler;
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
-
-set_exception_handler([new ErrorHandler(), 'handleException']);
 
 if (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
